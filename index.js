5824b7b0baa3ff8ce62e6e28e5055855ee9b5211
Jamel Seagraves <jseagra5@ford.com>
2019-02-12 08:47:25 -0800

    switched to pg client

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 const constants = require('./constants');
 const { MongoClient } = require('mongodb');
-const { Pool } = require('pg');
+const { Client } = require('pg');
 const url = 'mongodb://'+constants.MONGO_HOST+':'+constants.MONGO_PORT;
-const pgPool = new Pool({
+const pgClient = new Client({
 	user: constants.PG_USERNAME,
 	password: constants.PG_PASSWORD,
 	host: constants.PG_HOST,
     port: constants.PG_PORT,
-    database: constants.PG_DB_NAME,
-    connectionTimeoutMillis: 2000
+    database: constants.PG_DB_NAME
 });
 
 function migrateMetadata(client, users, index) {
 	client.query('UPDATE users SET metadata = '+ users[index].metadata +'WHERE id = ' + users[index]._id, (err, result) => {
 		if (err) throw err;
 		if (index >= 1) {
-			client.release();
+			client.end();
 			return console.log('Done!');
 		}
 		migrateMetadata(client, users, index+1);
@@ -29,19 +28,10 @@ MongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
 	const query = { zoneId: 'kcm' };
 	db.collection(constants.MONGO_COLLECTION_NAME).find(query).toArray((err, result) => {
 		if (err) throw err;
-		console.log(JSON.stringify({
-			user: constants.PG_USERNAME,
-			password: constants.PG_PASSWORD,
-			host: constants.PG_HOST,
-		    port: constants.PG_PORT,
-		    database: constants.PG_DB_NAME,
-		    connectionTimeoutMillis: 2000
-		}));
-		pgPool.connect((err, client, release) => {
-			if (err) throw err;
-			console.log('Connected to postgres database');
-			migrateMetadata(client, result, 0)
-		});
+		pgClient.connect();
+		console.log('Connected to postgres database');
+		migrateMetadata(pgClient, result, 0);
 		client.close();
+		console.log('Closed mongo client');
 	});
 });
