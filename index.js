b55da30d73c206309f0f73774a684939ec413bd1
Jamel Seagraves <jseagra5@ford.com>
2019-02-12 09:16:31 -0800

    stringified metadata

--- index.js
+++ index.js
@@ -11,8 +11,7 @@ const pgClient = new Client({
 });
 
 function migrateMetadata(client, users, index) {
-	console.log(users[index].metadata);
-	client.query('UPDATE users SET metadata = '+ users[index].metadata +'WHERE id = ' + users[index]._id, (err, result) => {
+	client.query('UPDATE users SET metadata = '+ JSON.stringify(users[index].metadata) +'WHERE id = ' + users[index]._id, (err, result) => {
 		if (err) throw err;
 		if (index >= 1) {
 			client.end();
